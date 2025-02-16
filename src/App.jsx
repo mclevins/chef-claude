3ffba81d22027aae42df70d89f6661e535e4c532
mclevins <mclevins@gmail.com>
2025-02-15 20:45:42 -0500

    added considerable styling to ingredients list and CTA to process AI async call to Claude

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,17 @@ function App() {
           />
           <button>Add Ingredient</button>
         </form>
-        <ul>
-          {ingredientListItems}
-        </ul>
+        {ingredients.length > 0 && <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">{ingredientListItems}</ul>
+          <div className="get-recipe-container">
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button>Get a recipe</button>
+          </div>
+        </section>}
       </main>
     </>
   )
